7ed32749fbbec50da32f34de6542917dc2405dab
fyofio <muhammadisrofiordi@gmail.com>
2022-03-04 03:03:19 +0700

    fix seeder url

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,8 +2,9 @@ pages << {
   page_type: "listings",
   method: "GET",
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-  url: "https://www.amazon.com/s?rh=n%3A6459737011&fs=true&ref=lp_6459737011_sar",
+  url: "https://www.amazon.com/s?k=LED+%26+LCD+TVs&i=electronics&rh=n%3A6459737011&qid=1629141285&ref=sr_pg_1",
   vars: {
     category: "LED & LCD TVs"
-  }
+  },
+  fetch_type: "browser"
 }
